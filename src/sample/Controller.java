df8c53c7ce8144587e42a979e1bb358d74ed5e5b
Hony Rodák <hony.com@seznam.cz>
2019-11-18 22:19:04 +0100

    Added Load and save image, about and exit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,23 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,6 +41,8 @@ public class Controller implements Initializable {
     @FXML
     private ImageView imageView;
 
+    private Stage window = Main.getPrimaryStage();
+
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
         this.applyMatrixFilter.setDisable(true);
@@ -43,4 +53,61 @@ public class Controller implements Initializable {
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
+
+    @FXML
+    public void showAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText("FAKE PHOTOSHOP");
+        String s = "SOME INFO ABOUT APP";
+        alert.setContentText(s);
+        alert.show();
+    }
+
+    @FXML
+    public void exit() {
+        Stage stage = (Stage) window.getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    public void openImage() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files",
+                        "*.bmp", "*.png", "*.jpg", "*.gif"));
+        File file = chooser.showOpenDialog(new Stage());
+        if (file != null) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                imageView.setImage(image);
+                imageView.fitHeightProperty().bind(window.heightProperty());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText("Please Select a File");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    public void saveImage() {
+        FileChooser stegoImageSaver = new FileChooser();
+        stegoImageSaver.setTitle("Save Image File");
+        File file = stegoImageSaver.showSaveDialog(null);
+        System.out.println(file.getAbsolutePath());
+        BufferedImage bImage = SwingFXUtils.fromFXImage(imageView.getImage(), null);
+        try {
+            ImageIO.write(bImage, ".jpg", file.getAbsoluteFile());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,11 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static Stage primaryStage;
+
+    static public Stage getPrimaryStage() {
+        return Main.primaryStage;
+    }
+
+    private void setPrimaryStage(Stage stage) {
+        Main.primaryStage = stage;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        setPrimaryStage(primaryStage);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setScene(new Scene(root, 900, 650));
+        primaryStage.setScene(new Scene(root, 1155, 650));
         primaryStage.setMinHeight(650);
         primaryStage.setMinWidth(900);
         primaryStage.show();
